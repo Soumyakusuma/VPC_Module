58836c19d49a6c03f5bf0a860b847c1ff16666b2
Soumyakusuma <soumyakusuma2@gmail.com>
2025-06-24 21:54:01 +0530

    nginx install

--- vpc_module_test/vpc.tf
+++ vpc_module_test/vpc.tf
@@ -1,6 +1,6 @@
 module "vpc_test"{
     #source="../vpc_module"
-    source="git::https://github.com/Soumyakusuma/VPC_Module.git?cd ref=main"
+    source="git::https://github.com/Soumyakusuma/VPC_Module.git?ref=main"
     environment=var.environment
     project=var.project
     pub_cidr=var.pub_cidr
